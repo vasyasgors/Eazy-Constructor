18f16dd0fd7b37790cd410a2b935c9b66e6c6470
Vasily <vasyasgors@gmail.com>
2023-02-23 01:49:57 +0300

    -m

--- Assets/EasyConstructor/Scripts/Actions/Instances/ToggleComponent.cs
+++ Assets/EasyConstructor/Scripts/Actions/Instances/ToggleComponent.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public enum ComponentName
 {
     Behaviour,
+    Light,
     MeshRenderer,
     Collider,
-    Animator
+    Animator,
+    Image,
+    SpriteRenderer
 }
 
 [ActionPath("Instances/ToggleComponent")]
@@ -21,8 +25,10 @@ public partial class ToggleComponent : ActionBase
     {
 
         if (_component == ComponentName.Behaviour) gameObject.GetComponent<Behaviour>().enabled = state.ToBool(gameObject.GetComponent<Behaviour>().enabled);
+        if (_component == ComponentName.Light) gameObject.GetComponent<Light>().enabled = state.ToBool(gameObject.GetComponent<Light>().enabled);
         if (_component == ComponentName.MeshRenderer) gameObject.GetComponent<MeshRenderer>().enabled = state.ToBool(gameObject.GetComponent<MeshRenderer>().enabled);
         if (_component == ComponentName.Collider) gameObject.GetComponent<Collider>().enabled = state.ToBool(gameObject.GetComponent<Collider>().enabled);
-        if (_component == ComponentName.Animator) gameObject.GetComponent<Animator>().enabled = state.ToBool(gameObject.GetComponent<Animator>().enabled);
+        if (_component == ComponentName.Image) gameObject.GetComponent<Image>().enabled = state.ToBool(gameObject.GetComponent<Image>().enabled);
+        if (_component == ComponentName.SpriteRenderer) gameObject.GetComponent<SpriteRenderer>().enabled = state.ToBool(gameObject.GetComponent<SpriteRenderer>().enabled);
     }
 }
